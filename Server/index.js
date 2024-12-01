55ebf40beb4ef3b2f0cfc04c03bdeb05ed5c26e6
Nishant <mojonly813@gmail.com>
2024-12-01 14:29:56 +0530

    Inital Server Setup

---
+++ Server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const authRoutes = require("./Routes/AuthRoutes");
+
+const dbConnect = require("./config/connect");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// dotenv.config();
+require('dotenv').config({ path: '../.env' });
+const PORT = process.env.PORT || 4000;
+
+//database connect
+dbConnect();
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:5173","*"],
+    })
+);
+
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
+
+
+app.listen(PORT, () => {
+	console.log(`App is running at ${PORT}`);
+  });
